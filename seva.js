2ac9a9ec4e3e3ec1ee5d2935937eb8ef51b14908
atom <atom@yigong100.com>
2014-04-23 15:17:19 +0800

    update dom ready and event listener

--- seva.js
+++ seva.js
@@ -1,64 +1,60 @@
-    var readyList = [];
-    atom.ready = function(fn){
-        if(readyList){
-            fn.push(fn);
-        }else{
-            fn();
-        }
-    };
-//============================domReady机制===========================
-    var readyFn, ready = W3C ? "DOMContentLoaded" : "readystatechange";
-  
-    function fireReady() {
+/*
+ *  seva.js
+ *  author  yangzhi<helloyangzhi@foxmail.com>
+ *  seva framework
+ */
+;(function(w,u){
+
+    var W3C = w.document.dispatchEvent ? true: false,
+        readyList = [];
+    //seva framework core model
+    var seva = function(w,u){
+        return {
+            ready:function(fn){
+                if(readyList){
+                    readyList.push(fn);
+                }else{
+                    fn();
+                }
+            },
+            bind: W3C ? function(el,type,fn,isup){
+                 el.addEventListener(type,fn,!!isup);
+                 return fn;
+            }:function(el,type,fn){
+                el.attachEvent && el.attachEvent("on" + type,fn);
+                return fn;
+            }
+        };
+    }(); 
+
+    var readyFn,ready = window.addEventListener ? "DOMContentLoaded" : "readystatechange";
+
+    if(!document.readyState){
+        var readyState = DOC.readyState = "loading";
+    }
+
+    function fireReady (){
         for (var i = 0 , fn; fn = readyList[i++];){
             fn();
         }
         readyList = null;
-        fireReady = $.noop; //隋性函数，防止IE9二次调用_checkDeps
+        //fireReady = $.noop; //隋性函数，防止IE9二次调用_checkDeps
     }
 
-    function doScrollCheck() {
-        try { //IE下通过doScrollCheck检测DOM树是否建完
-            html.doScroll("left");
-            fireReady();
-        } catch (e) {
-            setTimeout(doScrollCheck);
-        }
-    }
-    ;
-    //在firefox3.6之前，不存在readyState属性
-    //http://www.cnblogs.com/rubylouvre/archive/2012/12/18/2822912.html
-    if (!DOC.readyState) {
-        var readyState = DOC.readyState = "loading";
-    }
-    if (DOC.readyState === "complete") {
-        fireReady(); //如果在domReady之外加载
-    } else {
-        $.bind(DOC, ready, readyFn = function() {
-            if (W3C || DOC.readyState === "complete") {
+    //如果是在domReady之后再加载的方法,那么立刻执行.
+    if(document.readyState === "complete"){
+        fireReady();
+    }else{
+        seva.bind(document,ready,readyFn = function(){
+            if(W3C || document.readyState == "complete"){
                 fireReady();
-                if (readyState) { //IE下不能改写DOC.readyState
-                    DOC.readyState = "complete";
+                if(readyState){
+                    document.readyState = "complete";
                 }
             }
         });
-        if (html.doScroll) {
-            try { //如果跨域会报错，那时肯定证明是存在两个窗口
-                if (self.eval === parent.eval) {
-                    doScrollCheck();
-                }
-            } catch (e) {
-                doScrollCheck();
-            }
-        }
     }
-    
-    //============================bind机制===========================
-    
-    bind: W3C ? function(el, type, fn, phase) {
-            el.addEventListener(type, fn, !!phase);
-            return fn;
-    } : function(el, type, fn) {
-            el.attachEvent && el.attachEvent("on" + type, fn);
-            return fn;
-    }
+
+    window.sv = window.seva =  seva;
+})(window,undefined);
+
