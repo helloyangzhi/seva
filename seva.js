fdfdba76057ec473e766aca6bc65fb6c038cc1f2
atom <atom@yigong100.com>
2014-05-01 11:41:01 +0800

    implement module load function

--- seva.js
+++ seva.js
@@ -17,11 +17,22 @@
             "[object RegExp]" : 'RegExp',
             "[object Object]" : 'object'
         },
-        toString = Object.prototype.toString;
-
+        toString = Object.prototype.toString,
+        //节点列表
+        nodes = [],
+        //加载模块
+        modules = [];
+        //load节点
     //seva framework core model
     var seva = function(){
         return {
+            /**
+             * @title       ready 
+             * @description domReady方法，类型jquery中的ready方法，提前执行方法，在dom结构生成调用
+             * @param       fn      回调事件 
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
             ready:function(fn){
                 if(readyList){
                     readyList.push(fn);
@@ -29,6 +40,17 @@
                     fn();
                 }
             },
+
+            /*
+             * @title       bind 
+             * @description 绑定事件方法
+             * @param       el      元素
+             * @param       type    事件类型
+             * @param       fn      回调事件 
+             * @param       isup    是否采用冒泡方式绑定事件 
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
             bind: W3C ? function(el,type,fn,isup){
                  el.addEventListener(type,fn,!!isup);
                  return fn;
@@ -36,26 +58,183 @@
                 el.attachEvent && el.attachEvent("on" + type,fn);
                 return fn;
             },
+
+            /**
+             * @title       extend
+             * @description 扩展方法
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
             extend:function(){
             },
+
+            /**
+             * @title       isFunction
+             * @description 检测对象是否为函数
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
             isFunction:function(obj){
                return seva.type(obj) === 'function';
             },
+  
+            /**
+             * @title       isArray
+             * @description 检测对象是否为数组
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
             isArray: Array.isArray || function(obj){
                return seva.type(obj) === 'array';
             },
+
+            /**
+             * @title       isNaN
+             * @description 判断是否为NaN类型
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
             isNaN:function(obj){
                 return obj !== null && obj !== obj;
             },
+            
+            /**
+             * @title       type 
+             * @description 验证数据类型
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
             type: function(obj) {
                     return obj == null ?
                         String( obj ) :
                         class2type[ toString.call(obj) ] || "object";
             },
-            noop: function(){},
-            require: function(){
-                //需要一个require的方法
 
+            /**
+             * @title       noop
+             * @description 空方法，来自jquery,防止循环调用
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
+            noop: function(){},
+
+            /**
+             * @title       fireMethod
+             * @description 执行method,将modules中的回调方法都执行一遍
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
+            fireMethod:function(list,factory){
+                for(var i = 0 ; i < modules.length ; i ++){
+                    if(modules[i].id === list){
+                        modules[i].state = 2;
+                    }
+                }
+                factory();
+            },
+            /**
+             * @title       require
+             * @description 模块加载
+             * @param       list    依赖模块列表
+             * @param       factory 回调方法
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
+            require: function(list,factory){
+
+                //factory 匿名函数本身也是一个模块，这个给一个标识
+                var fn = "anonymous";
+
+                if(modules.length && modules[modules.length - 1].id === "anonymous"){
+                    seva.fireMethod(list,factory);
+                }else{
+                    //if list is array
+                    if(seva.isArray(list)){
+                        for(var i = 0,len = list.length;i<len;i++){
+                            //nodes.push(list[i]);
+                            nodes.push(list[i]);
+                            modules[i] = {
+                                "id":list[i],
+                                "state" :1
+                            };
+                        }
+                    }
+                    modules.push({
+                        "id" : "anonymous",
+                        "fn" : factory,
+                        "state" : 1
+                    });
+                }
+
+                if(seva.checkSetup()){
+                    modules[modules.length-1].fn();
+                }else{
+                    seva._loadJs();
+                }
+            },
+            /**
+             * @title       checkSetup 
+             * @description 检查所有模块是否完成正常安装
+             * @author      yangzhi<helloyangzhi@foxmail.com>
+             * @version     1.0
+             */
+            checkSetup:function(){
+                var isture = true;
+                for(var i =0 ; i < modules.length - 1;i++){
+                   if(modules[i].state === 1){
+                       isture = false;
+                       break;
+                   }
+                }
+                return isture;
+            },
+            /*
+             * @title       define
+             * @description 定义模块
+             * @author      yangzhi<helloyangzhi@foxmail.com> 
+             * @version     1.0
+             */
+            define:function(name,refers,factory){
+                seva.require(name,factory);
+            },
+            /*
+             * @title       _loadJs
+             * @description 加载js
+             * @author      yangzhi<helloyangzhi@foxmail.com> 
+             * @version     1.0
+             */
+            _loadJs:function(){
+                if(nodes.length){
+                    var cur = nodes.shift();
+                    if(cur !== "anonymous"){
+                        var script = seva.createScript();
+                        script.src= cur + ".js";
+                        document.getElementsByTagName("head")[0].appendChild(script);
+                        seva._loadJs();
+                    }
+                }
+            },
+            /*
+             * @title       _loadJs
+             * @description 动态创建js节点
+             * @author      yangzhi<helloyangzhi@foxmail.com> 
+             * @version     1.0
+             */
+            createScript:function(){
+                var script = document.createElement("script");
+                    script.type="text/javascript";
+                    script.charset="utf-8";
+                return script;
+            },
+            getBasePath:function(){
+                var result = "",src;
+                var script = document.getElementsByTagName("script");
+                for(var i = 0 ,el; el =script[i ++];){
+                    src = !! document.querySelcetor ? el.src :el.getAttribute("src",4);
+                    result = src;
+                    break;
+                }
+                return result.substr(0,result.lastIndexOf('/'));
             }
         };
     }(); 
